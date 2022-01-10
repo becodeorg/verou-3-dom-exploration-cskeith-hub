072a841056041781a66ecc344899dc6596ca5b36
Rustiek <cskeith01@gmail.com>
2022-01-10 10:50:38 +0100

    change background color

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ console.log(document.title);
 // Change title
 document.title = "Modifying the DOM" ;
 console.log(document.title);
+
+// change background-color
+document.body.style.backgroundColor = "red";
