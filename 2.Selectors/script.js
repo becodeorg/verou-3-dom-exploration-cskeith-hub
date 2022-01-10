58e8ef4f2431d0a3c93e958a43e62451401cf598
Rustiek <cskeith01@gmail.com>
2022-01-10 16:41:22 +0100

    W I P

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -3,6 +3,39 @@
 // Create a const that selects all important classes
 const important = document.querySelectorAll(".important");
 // Add a title to important
-important.title ="This is an important item";
+important.title = "This is an important item";
 // Console log the result
 console.log(important);
+
+
+//Select all the img tags and loop through them. If they have no important class, turn their display property to none
+
+// const that gets all images
+const img = document.querySelectorAll('img');
+// log images
+console.log(img);
+// for images in i log img
+for (let i = 0; i < img.length; i++) {
+    const currentImage = img[i];
+    console.log(img[i]);
+    // if currentImage is not equal to important display:none
+    if (currentImage.className != "important")
+        currentImage.style.display = "none";
+}
+
+//Loop through all the paragraphs and display their content in the console. If the paragraph has a class, display its classname has well
+
+// get all paragraphs
+const p = document.querySelectorAll('p');
+console.log(p);
+// for paragraph in i log p
+for (let i = 0; i < p.length; i++) {
+    const currentParagraph = p[i];
+    console.log(p[i]);
+    // if current p haves a className 
+    if (currentParagraph.className)
+        // console log currentParagraph with classname
+        console.log(currentParagraph.className);
+}
+//Give each of the paragraph a random text color (different for each one) UNLESS it has a class then leave it as it is.
+
