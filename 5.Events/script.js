4c86e4be7237f90854168c5bc92bf4a2351c396d
Rustiek <cskeith01@gmail.com>
2022-01-17 16:35:21 +0100

    wip

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -7,9 +7,42 @@ function getElapsedTime(){
 function clickOnSquare(e){
     console.log(e.target.classList[1])
     console.log(getElapsedTime())
+
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
-for(let actionsquare of actionsquares){
+for(let actionsquare of actionsquares) {
     actionsquare.addEventListener('click', clickOnSquare)
+
 }
+
+const displayedsquare = document.querySelector("displayedsquare-wrapper");
+
+const lime = document.createElement("div");
+lime.className = "displayedsquare";
+lime.style.backgroundColor = "lime";
+
+
+const magenta = document.createElement("div");
+magenta.className = "displayedsquare";
+magenta.style.backgroundColor = "magenta";
+
+
+const orange = document.createElement("div");
+orange.className = "displayedsquare";
+orange.style.backgroundColor = "orange";
+
+
+console.log(displayedsquare);
+
+
+
+
+
+
+
+
+
+
+
+
