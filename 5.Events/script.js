2da29758f3aeaea6eccfbc449079096146b8fec4
Rustiek <cskeith01@gmail.com>
2022-01-18 11:36:09 +0100

    Added time stamp & random bgColor

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -5,43 +5,44 @@ function getElapsedTime(){
 }
 
 function clickOnSquare(e){
-    console.log(e.target.classList[1])
-    console.log(getElapsedTime())
+    console.log(e.target.classList[1]);
+    console.log(getElapsedTime());
+
+    const displayedsquare = document.querySelector(".displayedsquare-wrapper");
+    const div = document.createElement("div");
+    div.classList.add("displayedsquare");
+    displayedsquare.append(div);
+    div.classList.add(e.target.classList[1]);
+
+    const logBox = document.querySelector("ul");
+    const log = document.createElement("li");
+    logBox.appendChild(log);
+    log.append( '[' + (getElapsedTime() + '] ') + (e.target.classList[1]));
 
 }
-
-const actionsquares = document.querySelectorAll('.actionsquare')
+const actionsquares = document.querySelectorAll('.actionsquare');
 for(let actionsquare of actionsquares) {
-    actionsquare.addEventListener('click', clickOnSquare)
+    actionsquare.addEventListener('click', clickOnSquare);
 
 }
 
-const displayedsquare = document.querySelector("displayedsquare-wrapper");
-
-const lime = document.createElement("div");
-lime.className = "displayedsquare";
-lime.style.backgroundColor = "lime";
 
 
-const magenta = document.createElement("div");
-magenta.className = "displayedsquare";
-magenta.style.backgroundColor = "magenta";
+const body = document.querySelector("body");
+body.addEventListener('keydown', random_bg_color);
 
+function random_bg_color(e) {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
+    if(e.which === 32) {
+        console.log("Hello world");
+        random_bg_color(body);
 
-const orange = document.createElement("div");
-orange.className = "displayedsquare";
-orange.style.backgroundColor = "orange";
-
-
-console.log(displayedsquare);
-
-
-
-
-
-
-
-
+    }
+    }
 
 
 
